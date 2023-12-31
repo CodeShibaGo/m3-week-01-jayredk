58ac9dfde5bd6f00f54a8f7c26cb396ce9bc3fa9
Jay <kred110396@gmail.com>
2023-12-31 22:33:55 +0800

    Add disemvowel

--- disemvowel.py
+++ disemvowel.py
@@ -1,2 +1,10 @@
 def disemvowel(s):
+    string = ''
+    vowel = ['a', 'e', 'i', 'o', 'u']
+    for char in s:
+        if char.lower() in vowel:
+            continue
+        string += char
+
+    return string
     pass
