1a61c953f8070448c77b3a68546a1b7b81500b15
Jay <kred110396@gmail.com>
2023-12-31 22:55:43 +0800

    Add remove_duplicates_from_list

--- remove_duplicates_from_list.py
+++ remove_duplicates_from_list.py
@@ -1,2 +1,12 @@
 def distinct(seq):
+    num_set = set()
+    arr = []
+    
+    for num in seq:
+        if num in num_set:
+            continue
+        else:
+            num_set.add(num)
+            arr.append(num)
+    return arr
     pass
