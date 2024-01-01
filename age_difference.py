b76a686b1d07750964b96344a8e6f5bf09b24c7f
Jay <kred110396@gmail.com>
2024-01-01 19:59:33 +0800

    Add age_difference

--- age_difference.py
+++ age_difference.py
@@ -1,2 +1,10 @@
 def age_difference(ages):
+    min = 100
+    max = 0
+    for age in ages:
+        if age > max:
+            max = age
+        if age < min:
+            min = age
+    return (min, max)
     pass
