7e8bcb134160badded99c9fa8851df9a0985e706
Jay <kred110396@gmail.com>
2024-01-01 20:23:25 +0800

    Add unique_in_order

--- unique_in_order.py
+++ unique_in_order.py
@@ -1,2 +1,15 @@
 def unique_in_order(iterable):
+    arr = []
+    if (len(iterable) == 0):
+        return arr
+        
+    arr = [iterable[0]]
+    index = 0
+
+    for el in iterable:
+        if el != arr[index]:
+            arr.append(el)
+            index += 1
+            
+    return arr
     pass
